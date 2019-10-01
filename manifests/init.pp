5bc200ee0e4f3d120316f4c9df66ea60526d0da0
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2019-10-01 18:06:03 +0900

    add group

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,13 @@
 class user_account(
+  $group = {},
   $user = {},
   $ssh_keygen = {},
 ){
+  $group.each |String $key, Hash $val| {
+    accounts::group {$key:
+      * => $val,
+    }
+  }
   $user.each |String $key, Hash $val| {
     accounts::user {$key:
       * => $val,
